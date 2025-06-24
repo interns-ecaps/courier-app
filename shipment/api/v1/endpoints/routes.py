5e84db338bc8fe929cecf4900ab5bd02ef86e96a
interns-ecaps <interns.ecaps@gmail.com>
2025-06-24 12:34:29 +0530

    ...

--- shipment/api/v1/endpoints/routes.py
+++ shipment/api/v1/endpoints/routes.py
@@ -133,49 +133,6 @@ def replace_shipment(
     return views.ShipmentService.replace_shipment(shipment_id, request, db)
 
 
-# =============================== PACKAGE =======================================
-
-
-@shipment_router.post("/create_package/")
-def create_package(request: CreatePackage, db: Session = Depends(get_db)):
-    return views.PackageService.create_package(request, db)
-
-
-@shipment_router.get("/packages/")
-def get_packages(
-    package_type: Optional[str] = Query(default=None),
-    currency_id: Optional[int] = Query(default=None),
-    is_negotiable: Optional[bool] = Query(default=None),
-    page: int = Query(default=1, ge=1),
-    limit: int = Query(default=10, ge=1),
-    db: Session = Depends(get_db),
-):
-    return views.PackageService.get_packages(
-        db=db,
-        package_type=package_type,
-        currency_id=currency_id,
-        is_negotiable=is_negotiable,
-        page=page,
-        limit=limit,
-    )
-
-
-@shipment_router.get("/packages/{package_id}", response_model=FetchPackage)
-def get_package_by_id(
-    package_id: int = Path(..., description="The ID of the package to retrieve"),
-    db: Session = Depends(get_db),
-):
-    return views.PackageService.get_package_by_id(package_id, db)
-
-
-@shipment_router.patch("/update_package/{package_id}")
-def patch_package(
-    package_id: int,
-    request: UpdatePackage = Body(...),
-    db: Session = Depends(get_db),
-):
-    print(request.dict(exclude_unset=False))
-    return views.PackageService.update_package(package_id, request, db)
 
 
 # ============================= STATUS TRACKER ===================================
--- shipment/api/v1/schemas/shipment.py
+++ shipment/api/v1/schemas/shipment.py
@@ -193,54 +193,6 @@ class ReplaceShipment(BaseModel):
         from_attributes = True
 
 
-# ======================= PACKAGE SCHEMAS ========================
-
-
-class CreatePackage(BaseModel):
-    package_type: PackageType
-    weight: float
-    length: float
-    width: float
-    height: float
-    is_negotiable: bool
-    currency_id: int
-    estimated_cost: Optional[float] = Field(None)
-    final_cost: Optional[float] = Field(None)
-
-    class Config:
-        from_attributes = True
-
-
-class FetchPackage(BaseModel):
-    id: int
-    package_type: PackageType
-    package_type: PackageType
-    weight: float
-    length: float
-    width: float
-    height: float
-    is_negotiable: bool
-    currency_id: int
-    is_deleted: bool
-
-    class Config:
-        from_attributes = True
-
-
-class UpdatePackage(BaseModel):
-    package_type: Optional[PackageType] = Field(None)
-    weight: Optional[float] = Field(None)
-    length: Optional[float] = Field(None)
-    width: Optional[float] = Field(None)
-    height: Optional[float] = Field(None)
-    is_negotiable: Optional[bool] = Field(None)
-    currency_id: Optional[int] = Field(None)
-    estimated_cost: Optional[float] = Field(None)
-    final_cost: Optional[float] = Field(None)
-    is_deleted: Optional[bool] = Field(None)
-
-    class Config:
-        from_attributes = True
 
 
 # ======================= STATUS SCHEMAS =======================
