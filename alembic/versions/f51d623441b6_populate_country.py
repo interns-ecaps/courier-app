9b3a0c20847b5a77c7210aa93eb33e5d868552fb
deonjose8778 <deonjose8778@gmail.com>
2025-07-09 17:17:14 +0530

    deon backend

--- alembic/versions/f51d623441b6_populate_country.py
+++ alembic/versions/f51d623441b6_populate_country.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'f51d623441b6'
-down_revision: Union[str, None] = '2680abea4fe0'
+down_revision: Union[str, None] = 'd5ccf4c33e30'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
